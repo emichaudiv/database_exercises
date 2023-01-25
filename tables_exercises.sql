654469a6d17d80bfb7e2b0bd6c74cb50916ff7ce
Edward Michaud <emichaudiv@gmail.com>
2023-01-25 15:30:15 -0600

    tables_exercises.sql

---
+++ tables_exercises.sql
@@ -0,0 +1,27 @@
+SHOW DATABASES;
+USE employees;
+SHOW TABLES;
+DESCRIBE employees;
+-- Which table(s) do you think contain a numeric type column? All but departments have intergers.
+Describe salaries;
+describe dept_emp;
+describe dept_manager;
+describe employees;
+describe titles;
+-- Which table(s) do you think contain a string type column? All but salaries have characters and varchars.
+describe dept_emp;
+describe dept_manager;
+describe employees;
+describe titles;
+describe departments;
+-- Which table(s) do you think contain a date type column? All but departments have dates.
+Describe salaries;
+describe dept_emp;
+describe dept_manager;
+describe employees;
+describe titles;
+-- What is the relationship between the employees and the departments tables? They have char and varchar.
+describe employees;
+describe departments;
+
+DeSCrIBE dept_manager;
