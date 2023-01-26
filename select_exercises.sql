4de010b203263b72c78a71379643c0644b953a37
Edward Michaud <emichaudiv@gmail.com>
2023-01-26 11:48:21 -0600

    select_exercises.sql

--- select_exercises.sql
+++ select_exercises.sql
@@ -2,18 +2,18 @@ use albums_db;
 describe albums;
 select * from albums;
 -- There are 31 rows
-select artist from albums;
--- There are 31 artists
-select name from albums;
--- Name is the primary key.
-select release_date from albums order by release_date asc;
+select distinct artist from albums;
+-- There are 23 artists
+describe albums;
+-- ID is the primary key.
+select min(release_date), max(release_date) from albums;
 -- Oldest 1967, Newest 2011
 select * from albums where artist = 'Pink Floyd';
 -- The Dark Side of The Moon, The Wall
-select * from albums where artist = 'The Beatles';
+select * from albums where name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
 -- 1967
-select * from albums where name = 'Nevermind';
--- Grunge
+select genre from albums where name = 'Nevermind';
+-- Grunge, Alternative rock
 select * from albums where release_date between 1990 and 1999;
 -- id no. 5,12,13,14,19,21,22,26,27,28,30
 select * from albums where sales < 20;
