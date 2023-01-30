a634b3d8494b6b191f70999e7dcb6bc2775edf99
Edward Michaud <emichaudiv@gmail.com>
2023-01-30 11:57:16 -0600

    functions_exercises.sql

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -9,7 +9,10 @@ from employees
 where (birth_date like '%-12-25') and (hire_date like '199%');
 
 describe salaries;
-select MIN(salary), MAX(salary) from salaries;
+select * from salaries
+where to_date = '9999-01-1' limit 10;
+select MIN(salary), MAX(salary) from salaries
+where to_date = '9999-01-1' limit 10;
 
 select first_name, last_name, birth_date, 
 LOWER(CONCAT((substr(first_name,1,1)),substr(last_name,1,4),'_', substr(birth_date,6,2),substr(birth_date,3,2))) as user_name
